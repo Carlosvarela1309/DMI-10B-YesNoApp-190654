a322fd4fa62890e860c49ac01598595fb3ec80fd
Carlosvarela1309 <163442089+Carlosvarela1309@users.noreply.github.com>
2024-11-23 14:52:17 -0600

    Add files via upload

--- flutter_application_1/lib/presentacion/chat/chat_screen.dart
+++ flutter_application_1/lib/presentacion/chat/chat_screen.dart
@@ -1,14 +1,8 @@
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_application_1/domain/entities/message.dart';
 
-import 'package:flutter_application_1/persentation/providers/chat_provider.dart';
-import 'package:flutter_application_1/persentation/widgets/chat/other_message_bubble.dart';
-import 'package:flutter_application_1/persentation/widgets/chat/my_message_bubble.dart';
-import 'package:flutter_application_1/persentation/widgets/shared/message_field_box.dart';
-
-class ChastScreen extends StatelessWidget {
-  const ChastScreen({super.key});
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,47 +11,11 @@ class ChastScreen extends StatelessWidget {
         leading: Padding(
           padding: const EdgeInsets.all(4.0),
           child: CircleAvatar(
-            backgroundImage: NetworkImage(
-                'https://i.pinimg.com/236x/09/93/53/09935324c7073b647dfbdfc3eb605982.jpg'),
+            backgroundImage: NetworkImage('https://media.canalnet.tv/2021/11/paola-danna-974x550.jpg'),
           ),
         ),
-        title: Text('Tommy😎'),
+        title: Text('Danna ❤️'),
         centerTitle: false,
-      ),
-      body: _ChatView(),
-    );
-  }
-}
-
-class _ChatView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final chatProvider = context.watch<ChatProvider>();
-
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-            Expanded(
-                child: ListView.builder(
-                    controller: chatProvider.chatScrollController,
-                    itemCount: chatProvider.messageList.length,
-                    itemBuilder: (context, index) {
-                      final message = chatProvider.messageList[index];
-
-                      return (message.fromWho == FromWho.hers)
-                          ? HerMessageBubble(message: message)
-                          : MyMessageBubble(message: message);
-                    })),
-
-            /// Caja de texto de mensajes
-            MessageFieldBox(
-              // onValue: (value) => chatProvider.sendMessage(value),
-              onValue: chatProvider.sendMessage,
-            ),
-          ],
-        ),
       ),
     );
   }
